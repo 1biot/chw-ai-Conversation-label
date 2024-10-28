26a5ba93e3dff02de57046aa226d86e0f5224615
Tomáš Lebeda <lebeda@users.noreply.github.com>
2024-10-28 13:54:32 +0100

    Using DotEnv only when is on dev env

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,8 +2,10 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
-
 const LOG_DIR = __DIR__ . '/../log';
+function isDev(): bool {
+    return _env('APP_ENV', 'production') === 'development';
+}
 
 use Dotenv\Exception\InvalidEncodingException;
 use Dotenv\Exception\InvalidFileException;
@@ -13,18 +15,20 @@ if (!is_dir(LOG_DIR)) {
     mkdir(LOG_DIR);
 }
 
-try {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-    $dotenv->safeLoad();
-    $dotenv->required(
-        ['OPENAI_API_KEY', 'OPENAI_ORG', 'OPENAI_ASSISTANT_ID', 'CHATWOOT_API_ACCESS_TOKEN', 'CHATWOOT_API_URL']
-    );
-} catch (InvalidEncodingException|InvalidFileException $e) {
-    response()->json([], Status::HTTP_INTERNAL_SERVER_ERROR, true);
+if (isDev()) {
+    try {
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+        $dotenv->safeLoad();
+        $dotenv->required(
+            ['OPENAI_API_KEY', 'OPENAI_ORG', 'OPENAI_ASSISTANT_ID', 'CHATWOOT_API_ACCESS_TOKEN', 'CHATWOOT_API_URL']
+        );
+    } catch (InvalidEncodingException|InvalidFileException $e) {
+        response()->json([], Status::HTTP_INTERNAL_SERVER_ERROR, true);
+    }
 }
 
 $app = new App\ChatwootListener([
-    'debug' => _env('APP_ENV', 'production') === 'development'
+    'debug' => isDev()
 ]);
 
 return $app;
